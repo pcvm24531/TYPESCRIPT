bcc3b0f8ddf900ebbc0a3af5c6724006333d12ed
Cesar <csr2453@outlook.es>
2024-03-21 16:44:02 -0400

    Ejemplo variable ANY

---
+++ any.ts
@@ -0,0 +1,12 @@
+/*Permite declarar variables, arreglos de cualquier tipo
+*Any viene a ser ualquier tipo de dato, en lo posible hay que evitar su uso
+*/
+let edad: any;
+edad = 12;
+console.log(edad);
+
+edad = "Doce años";
+console.log( edad );
+
+edad = true;
+console.log(edad);
